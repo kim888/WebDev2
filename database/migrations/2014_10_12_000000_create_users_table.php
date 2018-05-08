6580ed2e86394b58d2b5f402357cd5af7efefae4
Maximilian Lindig <maximilian.lindig@gmail.com>
2018-05-08 13:08:14 +0200

    User Migration und Seeder erstellt.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,21 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('lastname',30);
+            $table->string('surename', 30);
+            $table->string('city', 30);
+            $table->string('street', 35);
+            $table->integer('streetnumber');
+            $table->integer('zip');
+            $table->string('country');
+            $table->integer('telephone');
+            //$table->date('birthdate');
+
             $table->rememberToken();
             $table->timestamps();
+
         });
     }
 
--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class UserSeeder extends Seeder
 {
@@ -12,20 +13,20 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
-            'name' => str_random(10),
-            'email' => str_random(10).'@gmail.com',
-            'password' => bcrypt('secret'),
-        ]);
-        DB::table('users')->insert([
-            'name' => str_random(10),
-            'email' => str_random(10).'@gmail.com',
-            'password' => bcrypt('secret'),
-        ]);
-        DB::table('users')->insert([
-            'name' => str_random(10),
-            'email' => str_random(10).'@gmail.com',
-            'password' => bcrypt('secret'),
-        ]);
+        for ($x = 0; $x <= 10; $x++) {
+            DB::table('users')->insert([
+                'lastname' => str_random(10),
+                'surename' => str_random(10),
+                'email' => str_random(10).'@gmail.com',
+                'password' => bcrypt('secret'),
+                'city' => str_random(10),
+                'street' => str_random(10),
+                'streetnumber' => random_int(001, 999),
+                'zip' => random_int(11111,99999),
+                'country' => str_random(10),
+                'telephone' => random_int(1,9999),
+                //'date' => Carbon::parse('2000-01-01')
+            ]);
+        }
     }
 }
