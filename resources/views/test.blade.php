7a61bb691588df847832ef7e4d8d2079e553c64c
kim888 <kimberly.schlenzig@hof-university.de>
2018-05-27 19:16:09 +0200

    link geändert

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -124,6 +124,154 @@
 -->
 
 
+
+<!-- Email Formula Internet Recherche
+
+
+    <form name="contact_form" method="post"  onSubmit="return evalid()">
+        <table><tr>
+                <td>First Name *</td>
+                <td colspan="2"><input name="fname" type="text" /></td>
+            </tr><tr>
+                <td>Last Name *</td>
+                <td colspan="2"><input name="lname" type="text" /></td>
+            </tr><tr>
+                <td>Your E-mail *</td>
+                <td colspan="2"><input type="text" name="mail" /></td>
+            </tr><tr>
+                <td>Subject *</td>
+                <td colspan="2"><input name="subject" type="text" /></td>
+            </tr><tr>
+                <td>Message *</td>
+                <td colspan="2"><textarea name="message"></textarea></td>
+            </tr><tr>
+                <td>&nbsp;</td>
+                <td colspan="2">&nbsp;</td>
+            </tr>
+            <tr>
+                <td><input type="reset" name="reset" value="Reset"/></td>
+                <td align="right"><input type="submit" name="Submit" value="Submit"></td>
+            </tr>
+        </table>
+    </form>
+    <script type="text/javascript">
+        //email form validation
+
+        function everif(str) {
+
+            var at="@"
+            var punct="."
+            var lat=str.indexOf(at)
+            var lstr=str.length
+            var lpunct=str.indexOf(punct)
+            if (str.indexOf(at)==-1){
+                alert("Valid email must be entered")
+                return false
+            }
+
+            if (str.indexOf(at)==-1 || str.indexOf(at)==0 || str.indexOf(at)==lstr){
+                alert("Valid email must be entered")
+                return false
+            }
+
+            if (str.indexOf(punct)==-1 || str.indexOf(punct)==0 || str.indexOf(punct)==lstr){
+                alert("Valid email must be entered")
+                return false
+            }
+
+            if (str.indexOf(at,(lat+1))!=-1){
+                alert("Valid email must be entered")
+                return false
+            }
+
+            if (str.substring(lat-1,lat)==punct || str.substring(lat+1,lat+2)==punct){
+                alert("Valid email must be entered")
+                return false
+            }
+
+            if (str.indexOf(punct,(lat+2))==-1){
+                alert("Valid email must be entered")
+                return false
+            }
+
+            if (str.indexOf(" ")!=-1){
+                alert("Valid email must be entered")
+                return false
+            }
+
+            return true
+        }
+
+        function evalid(){
+            var emailID=document.contact_form.mail
+
+            if (everif(emailID.value)==false){
+                emailID.focus()
+                return false
+            }
+
+            //empty field validation
+
+            var fname=document.contact_form.fname
+            if ((fname.value==null)||(fname.value=="")){
+                alert("Fields marqued with * must be entered")
+                fname.focus()
+                return false
+            }
+
+            var lname=document.contact_form.lname
+            if ((lname.value==null)||(lname.value=="")){
+                alert("Fields marqued with * must be entered")
+                lname.focus()
+                return false
+            }
+
+            var subject=document.contact_form.subject
+            if ((subject.value==null)||(subject.value=="")){
+                alert("Fields marqued with * must be entered")
+                subject.focus()
+                return false
+            }
+
+            var message=document.contact_form.message
+            if ((message.value==null)||(message.value=="")){
+                alert("Fields marqued with * must be entered")
+                message.focus()
+                return false
+            }
+
+            return true
+        }
+    </script>
+    <?php session_start();
+    if(isset($_POST['Submit'])) {
+        $youremail = 'kimberly.schlenzig@hof-university.de';
+        $fromsubject = 'kimberly.schlenzig@hof-university.de';
+        $fname = $_POST['fname'];
+        $lname = $_POST['lname'];
+        $mail = $_POST['mail'];
+        $subject = $_POST['subject'];
+        $message = $_POST['message'];
+        $to = $youremail;
+        $mailsubject = 'Masage recived from'.$fromsubject.' Contact Page';
+        $body = $fromsubject.'
+
+	The person that contacted you is  '.$fname.' '.$lname.'
+	 E-mail: '.$mail.'
+	 Subject: '.$subject.'
+
+	 Message:
+	 '.$message.'
+
+	|---------END MESSAGE----------|';
+        echo "Thank you fo your feedback. I will contact you shortly if needed.<br/>Go to <a href='/index.php'>Home Page</a>";
+        mail($to, $subject, $body);
+    } else {
+        echo "You must write a message. </br> Please go to <a href='/contact.php'>Contact Page</a>";
+    }
+    ?>
+    -->
+
 </div>
 
 
