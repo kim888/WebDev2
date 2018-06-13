fbabb51035fb83bdcc087e5710f88f3d9ae67997
kim888 <kimberly.schlenzig@hof-university.de>
2018-06-13 12:37:15 +0200

    Profil/ logout/ warenkorb button in nav bzw je nachdem ob guest oder auth + mail dateien

---
+++ app/Mail/contacts.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class contacts extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from('kimberly.schlenzig@hof-university.de')
+            ->view('emails.orders.shipped')
+            ->text('emails.orders.shipped_plain');
+    }
+}
