5a8f0cb741b7926a29a3196019e7fd892a5aabaa
Thomas Buchholtz <>
2018-07-04 10:19:35 +0200

    Voller Warenkorb

---
+++ resources/views/vollerWarenkorb.blade.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Warenkorb</title>
+    <link rel="stylesheet" href="{{asset("assets/bootstrap/css/bootstrap.min.css")}}">
+    <link rel="stylesheet" href="{{asset("assets/fonts/font-awesome.min.css")}}">
+    <link rel="stylesheet" href="{{asset("assets/fonts/ionicons.min.css")}}">
+    <link rel="stylesheet" href="{{asset("assets/css/Carousel-Hero.css")}}">
+    <link rel="stylesheet" href="{{asset("assets/css/Features-Boxed.css")}}">
+    <link rel="stylesheet" href="{{asset("assets/css/Footer-Basic.css")}}">
+    <link rel="stylesheet" href="{{asset("https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css")}}">
+    <link rel="stylesheet" href="{{asset("https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.3.1/css/swiper.min.css")}}">
+    <link rel="stylesheet" href="{{asset("assets/css/Login-Form-Dark.css")}}">
+    <link rel="stylesheet" href="{{asset("assets/css/Media-Slider-Bootstrap-3.css")}}">
+    <link rel="stylesheet" href="{{asset("assets/css/Media-Slider-Bootstrap-3.css")}}">
+    <link rel="stylesheet" href="{{asset("assets/css/Pretty-Registration-Form.css")}}">
+    <link rel="stylesheet" href="{{asset("assets/css/Navigation-with-Search.css")}}">
+    <link rel="stylesheet" href="{{asset("assets/css/Profile-Edit-Form.css")}}">
+    <link rel="stylesheet" href="{{asset("assets/css/Profile-Edit-Form.css")}}">
+    <link rel="stylesheet" href="{{asset("assets/css/Registration-Form-with-Photo.css")}}">
+    <link rel="stylesheet" href="{{asset("assets/css/Simple-Slider.css")}}">
+    <link rel="stylesheet" href="{{asset("assets/css/styles.css")}}">
+
+    <link rel="shortcut icon" type="image/x-icon" href="{{asset("/img/FavIcon2.0.png")}}">
+</head>
+
+<body>
+<nav class="navbar navbar-light navbar-expand-md navigation-clean-search" style="background-color:rgba(52,54,66,0);padding-left:16px;">
+    <div class="container"><a class="navbar-brand" href="index.html" style="background-image:url({{asset('/img/Logo.png')}});max-width:none;width:160px;height:80px;background-repeat:no-repeat;background-size:contain;"></a><button class="navbar-toggler" data-toggle="collapse"
+                                                                                                                                                                                                                                      data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse" id="navcol-1">
+            <ul class="nav navbar-nav">
+                <li class="nav-item" role="presentation"><a class="nav-link" href="featured.html">Featured</a></li>
+                <li class="nav-item" role="presentation"><a class="nav-link" href="genre.html">Genre</a></li>
+                <li class="nav-item" role="presentation"><a class="nav-link" href="location.html">Location</a></li>
+            </ul>
+            <form class="form-inline mr-auto" target="_self">
+                <div class="form-group"><label for="search-field"><i class="fa fa-search"></i></label><input class="form-control search-field" type="search" name="search" data-bs-hover-animate="pulse" id="search-field"></div>
+            </form><a class="btn btn-primary" role="button" href="warenkorb.html" style="width:40px;height:40px;margin:0px;padding-top:6px;background-image:url(&quot;assets/img/Warenkorb.png&quot;);background-size:contain;background-repeat:no-repeat;background-color:rgba(255,255,255,0);color:rgb(255,255,255);"></a>
+            <a
+                    class="btn btn-light action-button" role="button" href="signUp.html" style="background-color:#979c9c;">Sign Up</a><a class="btn btn-light action-button" role="button" href="logIn.html">Login</a></div>
+    </div>
+</nav>
+<div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3"><img src="({{asset('/img/justin_timberlake_2.jpg')}})" style="width:150px;padding:16px;padding-top:16px;padding-right:16px;padding-bottom:16px;padding-left:16px;"></div>
+            <div class="col-md-3">
+                <p style="font-size:16px;width:180px;color:white;margin-top:50px;">Justin Timberlake</p>
+            </div>
+            <div class="col" style="min-width:100px;max-width:120px;">
+                <p style="font-size:16px;color:rgb(255,255,255);margin-top:50px;">Platz: 14 B</p>
+            </div>
+            <div class="col-md-3" style="min-width:120px;max-width:120px;">
+                <p style="font-size:16px;color:rgb(255,255,255);margin-top:50px;">Menge: 1</p>
+            </div>
+            <div class="col-md-3">
+                <div class="dropdown show"><button class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-expanded="true" type="button" style="border-radius:20px;background-color:#348899;margin-left:40px;margin-top:50px;">Bearbeiten</button>
+                    <div class="dropdown-menu show"
+                         role="menu" style="border-radius:40px;background-color:#979c9c;"><a class="dropdown-item" role="presentation" href="#" style="background-color:#979c9c;border-radius:20px;color:red;">Löschen</a></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3"></div>
+            <div class="col-md-3"></div>
+            <div class="col-md-3"></div>
+            <div class="col-md-3"><button class="btn btn-primary" type="button" style="margin-top:50px;margin-left:40px;border-radius:20px;background-color:#348899;">Zur Kasse</button></div>
+        </div>
+    </div>
+</div>
+<div class="footer-basic">
+    <footer>
+        <div class="social"><a href="http://www.instagram.com"><i class="icon ion-social-instagram"></i></a><a href="http://www.snapchat.de"><i class="icon ion-social-snapchat"></i></a><a href="http://www.twitter.de"><i class="icon ion-social-twitter"></i></a><a href="http://www.facebook.de"><i class="icon ion-social-facebook"></i></a></div>
+        <ul
+                class="list-inline">
+            <li class="list-inline-item"><a href="#">Home</a></li>
+            <li class="list-inline-item"><a href="#">Services</a></li>
+            <li class="list-inline-item"><a href="#">About</a></li>
+            <li class="list-inline-item"><a href="#">Terms</a></li>
+            <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
+        </ul>
+        <p class="copyright">NEITQEY © 2018</p>
+    </footer>
+</div>
+<script src="../../../bootstrap/assets/js/jquery.min.js"></script>
+<script src="../../../bootstrap/assets/bootstrap/js/bootstrap.min.js"></script>
+<script src="../../../bootstrap/assets/js/bs-animation.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.3.1/js/swiper.jquery.min.js"></script>
+<script src="../../../bootstrap/assets/js/Media-Slider-Bootstrap-3.js"></script>
+<script src="../../../bootstrap/assets/js/Profile-Edit-Form.js"></script>
+<script src="../../../bootstrap/assets/js/Simple-Slider.js"></script>
+</body>
+
+</html>
